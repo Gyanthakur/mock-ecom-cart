857f5ee3fbd90811c8059d76ab5e6d58a3a440f2
Gyanthakur <gps.96169@gmail.com>
2025-10-29 01:34:27 +0530

    Update API proxy URL to point to the production backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000' 
+      '/api': 'https://mock-ecom-cart-backend.onrender.com' 
+       
     }
   }
 });
